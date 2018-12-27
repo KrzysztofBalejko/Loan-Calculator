11dedee833af2c7126b000ec6ab5732f7abf52f7
KrzysztofBalejko <ec2-user@ip-172-31-21-6.eu-west-1.compute.internal>
2018-12-27 16:35:51 +0000

    Changes to loading display

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 function calculateResults(e) {
   const amount = document.getElementById('amount');
@@ -19,21 +25,23 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+    document.getElementById('results').style.display = 'block';
+    document.getElementById('loading').style.display = 'none';
   } else {
     showError('Please check your numbers');
   }
-
-  e.preventDefault();
 }
 
 function showError(error){
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('loading').style.display = 'none';
   const errorDiv = document.createElement('div');
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
   errorDiv.className = 'alert alert-danger';
   errorDiv.appendChild(document.createTextNode(error));
   card.insertBefore(errorDiv, heading);
-  setTimeout(clearError, 3000)
+  setTimeout(clearError, 3000);
 }
 
 function clearError() {
